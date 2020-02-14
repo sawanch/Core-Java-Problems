d7496eda54ff2e94ba3adec4318a582e37be5cb8
sawanch <sawanchakraborty100@gmail.com>
2020-02-14 23:50:28 +0530

    Hello World Program

---
+++ Hello World/src/com/sawan/Practice.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package com.sawan;
+
+/**
+ * @author win10
+ *
+ */
+public class Practice {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+
+	}
+
+}
