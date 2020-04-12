19cedb2ea19540659cc05aab6e54b78ba868c5c7
sawanch <sawanchakraborty100@gmail.com>
2020-04-12 00:11:02 -0700

    commit

---
+++ generate-graph.py
@@ -0,0 +1,27 @@
+import os
+from random import randint
+from datetime import datetime, timedelta
+
+# Set your desired start and end dates in yyyy-mm-dd format and the maximum number of commits per day
+start_date_str = "2020-04-11"
+end_date_str = "2020-04-25"
+max_commits_per_day = 3
+
+# Convert start and end date strings to datetime objects
+start_date = datetime.strptime(start_date_str, "%Y-%m-%d")
+end_date = datetime.strptime(end_date_str, "%Y-%m-%d")
+
+# Loop through the date range
+current_date = start_date
+while current_date < end_date:
+    # Generate a random number of commits for the current day, up to the specified maximum
+    for _ in range(0, randint(0, max_commits_per_day)):
+        current_day_str = current_date.strftime("%Y-%m-%d")
+        with open('file.txt', 'a') as file:
+            file.write(current_day_str + '\n')  # Add a newline character for better readability
+        os.system('git add .')
+        os.system('git commit --date="{}" -m "commit"'.format(current_day_str))
+
+    current_date += timedelta(days=1)
+
+os.system('git push -u origin master ')
